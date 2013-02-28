be45f9ecd1fa6ac3872afb9fc588db5c5be74f13
Scott Cantisani <scantisani@gmail.com>
2013-02-28 05:14:32 +0000

    first draft of generator, changes to solver

--- Sudoku.java
+++ Sudoku.java
@@ -9,60 +9,35 @@
 *				Oana Radu
 *
 * Creation date:	06.02.2013
-* Last updated:		18.02.2013
+* Last updated:		28.02.2013
 ***********************************************/
+
+import java.util.Random;
+
 public class Sudoku {
 
+	//board is for solving and testing
 	private int[][] board = new int[9][9];
+	//srcBoard is a board to backtrack to if something goes wrong
 	private int[][] srcBoard = new int[9][9];
 
 	public Sudoku () {
-		initBoard();
+		initBoard(2);
 		printBoard();
 		System.out.println();
-		if (multSol() || !solveBoard()) {
-			System.out.println("Invalid sudoku");
-		} else {
-			printBoard();
-		}
+		solveBoard();
+		printBoard();
 	}
 
-	public void initBoard () {
-		int[][] temp = {
-//			{4, 0, 0, 0, 8, 0, 5, 0, 6},
-//			{0, 8, 0, 6, 0, 2, 0, 0, 4},
-//			{0, 0, 6, 0, 9, 5, 0, 1, 0},
-//			{0, 9, 1, 0, 0, 0, 0, 4, 0},
-//			{0, 0, 0, 8, 4, 9, 0, 0, 0},
-//			{0, 5, 0, 0, 0, 0, 3, 8, 0},
-//			{0, 7, 0, 5, 2, 0, 8, 0, 0},
-//			{9, 0, 0, 3, 0, 7, 0, 2, 0},
-//			{5, 0, 2, 0, 1, 0, 0, 0, 3}
-			{0, 5, 4, 0, 0, 0, 0, 9, 0},
-			{0, 1, 3, 0, 9, 7, 0, 0, 0},
-			{0, 0, 2, 0, 0, 0, 0, 0, 0},
-			{7, 0, 0, 6, 0, 0, 2, 1, 0},
-			{0, 0, 0, 5, 0, 9, 0, 0, 0},
-			{0, 2, 5, 0, 0, 1, 0, 0, 9},
-			{0, 0, 0, 0, 0, 0, 1, 0, 0},
-			{0, 0, 0, 4, 1, 0, 9, 3, 0},
-			{0, 6, 0, 0, 0, 0, 8, 5, 0}
-//			{1, 0, 0, 0, 0, 0, 0, 0, 0},
-//			{0, 0, 0, 0, 0, 0, 0, 0, 0},
-//			{0, 0, 0, 0, 0, 0, 0, 0, 0},
-//			{0, 0, 0, 0, 0, 0, 0, 0, 0},
-//			{0, 0, 0, 0, 0, 0, 0, 0, 0},
-//			{0, 0, 0, 0, 0, 0, 0, 0, 0},
-//			{0, 0, 0, 0, 0, 0, 0, 0, 0},
-//			{0, 0, 0, 0, 0, 0, 0, 0, 0},
-//			{0, 0, 0, 0, 0, 0, 0, 0, 0}
-		};
-
-		for (int i = 0; i < 9; i++) {
-			for (int j = 0; j < 9; j++) {
-				board[i][j] = temp[i][j];
-				srcBoard[i][j] = temp[i][j];
-			}
+	public void initBoard (int diff) {
+		genBoard();
+		Random random = new Random();
+		if (diff == 1) {
+			dig(30 + random.nextInt(10));
+		} else if (diff == 2) {
+			dig(40 + random.nextInt(10));
+		} else {
+			dig(50 + random.nextInt(10));
 		}
 	}
 
@@ -97,11 +72,6 @@ public class Sudoku {
 	}
 
 	private boolean solveBoard (int i, int j, int n) {
-
-		int[] col = new int[9];
-		int[] row = new int[9];
-		int[] block = new int[9];
-
 		if (i == 9 && j == 0) {
 			//if at end of board, puzzle solved
 			return true;
@@ -109,11 +79,7 @@ public class Sudoku {
 			if (board[i][j] == 0) {
 				//if the cell is empty
 				while (n <= 9) {
-					col = getCol(j);
-					row = getRow(i);
-					block = getBlock(i, j);
-
-					if (contains(col, n) || contains(row, n) || contains(block, n)) {
+					if (contains(i, j, n)) {
 						//find the first possible number for the cell
 						n++;
 					} else {
@@ -133,64 +99,44 @@ public class Sudoku {
 				//if no possible numbers lead to a solution, return false
 				return false;
 			} else {
-				//if not, check the validity of the number already in the cell
-				n = board[i][j];
-				board[i][j] = 0;
-				
-				col = getCol(j);
-				row = getRow(i);
-				block = getBlock(i, j);
-
-				if (contains(col, n) || contains(row, n) || contains(block, n)) {
-					//if the column, row, or block already contain this number, return false
-					board[i][j] = n;
-					return false;
-				}
-				else {
-					//if not, keep solving the board
-					board[i][j] = n;
-					return solveBoard(next(i, j)[0], next(i, j)[1], 1);
-				}
+				//if not, go to the next cell
+				return solveBoard(next(i, j)[0], next(i, j)[1], 1);
 			}
 		}
 	}
 
 	public boolean multSol () {
 		//checks for multiple solutions by brute force. Definitely not the best way to do this but using it for now to test the generator
+		int n = 0;
 		for (int i = 0; i < 9; i++) {
 			for (int j = 0; j < 9; j++) {
-
 				if (srcBoard[i][j] == 0) {
 					//if a cell in the original puzzle is empty
-					int n = 0;
 					for (int k = 1; k < 10; k++) {
 						//loop through all possible numbers for this cell
-						resetBoard(0, 0);
+						resetBoard();
 						board[i][j] = k;
-						if (solveBoard()) {
+						if (!contains(i, j, k) && solveBoard()) {
 							//count all possible solutions for numbers in this cell
 							n++;
 						}
 					}
 
 					if (n > 1) {
-						//if there is a possible solution for more than one number, return true
+						//if there is more than one possible solution, return true
 						return true;
 					}
 					board[i][j] = 0;
 				}
 			}
 		}
+		//if after testing all possible numbers for all empty cells there is not more than one solution, return false
 		return false;
 	}
 
-	public void resetBoard (int i, int j) {
-		//resets the puzzle from a certain cell to the end of the board
-		for (j = j; j < 9; j++) {
-			board[i][j] = srcBoard[i][j];
-		}
-		for (i = i + 1; i < 9; i++) {
-			for (j = 0; j < 9; j++) {
+	public void resetBoard () {
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
 				board[i][j] = srcBoard[i][j];
 			}
 		}
@@ -240,6 +186,18 @@ public class Sudoku {
 		return block;
 	}
 
+	public boolean contains(int i, int j, int n) {
+		int[] col = getCol(j);
+		int[] row = getRow(i);
+		int[] block = getBlock(i, j);
+
+		if(contains(col, n) || contains(row, n) || contains(block, n)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
 	public boolean contains (int[] array, int n) {
 		for (int i = 0; i < array.length; i++) {
 			if (array[i] == n) {
@@ -247,6 +205,79 @@ public class Sudoku {
 			}
 		}
 		return false;
+	}
+
+//	public boolean valid () {
+		//this function is unnecessary right now--was used before for testing the generator, might come in handy again later?
+//		int n;
+//
+//		for (int i = 0; i < 9; i++) {
+//			for (int j = 0; j < 9; j++) {
+//				if (board[i][j] != 0) {
+//					n = board[i][j];
+//					board[i][j] = 0;
+//					
+//					if (contains(i, j, n)) {
+//						board[i][j] = n;
+//						return false;
+//					} else {
+//						board[i][j] = n;
+//					}
+//				}
+//			}
+//		}
+//		return true;
+//	}
+	
+	private int random () {
+		Random rand = new Random();
+		return rand.nextInt(9);
+	}
+
+	private void genBoard () {
+		//generates a valid filled sudoku puzzle
+		int i, j, n;
+
+		do {
+			resetBoard();
+			//fill the board up randomly with numbers, then attempt to solve it
+			//80 attempts at filling a cell (the actual number of filled cells will be 35-45) seems to make for a good balance between solve speed (faster if there are more filled cells) and randomness of puzzles generated
+			for (int k = 0; k < 80; k++) {
+				i = random();
+				j = random();
+				n = random() + 1;
+
+				if (!contains(i, j, n)) {
+					board[i][j] = n;
+				}
+			}
+		} while (!solveBoard());
+
+		//once a solution is found, set srcBoard to the solved board
+		for (i = 0; i < 9; i++) {
+			for (j = 0; j < 9; j++) {
+				srcBoard[i][j] = board[i][j];
+			}
+		}
+	}
+
+	private void dig (int n) {
+		//make n randomly placed cells empty
+		int i, j, k, dug = 0;
+
+		while (dug < n) {
+			i = random();
+			j = random();
+			if (board[i][j] != 0) {
+				k = board[i][j];
+				board[i][j] = 0;
+				if (!multSol()) {
+					dug++;
+				} else {
+					board[i][j] = k;
+				}
+			}
+		}
 	}
 
 	public static void main (String[] args) {
